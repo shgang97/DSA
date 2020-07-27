ad4fde43aa803c9f1bfea6040263be2e93c08889
shgang <shgang97@163.com>
2020-07-27 23:53:06 +0800

    add top ten algorithm

---
+++ JCodes/src/com/shgang/algorithm/prim/PrimAlgorithm.java
@@ -0,0 +1,85 @@
+package com.shgang.algorithm.prim;
+
+import java.util.Arrays;
+
+public class PrimAlgorithm {
+	public static void main(String[] args) {
+		char[] data = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+		int verxs = data.length;
+		int[][] weight = new int[][]{
+				{10000, 5, 7, 10000, 10000, 10000, 2},
+				{5, 10000, 10000, 9, 10000, 10000, 3},
+				{7, 10000, 10000, 10000, 8, 10000, 4, 10000},
+				{10000, 9, 10000, 10000, 10000, 4, 10000},
+				{10000, 10000, 8, 10000, 10000, 5, 4},
+				{10000, 10000, 10000, 4, 5, 10000, 6},
+				{2, 3, 10000, 10000, 4, 6, 10000},
+		};
+		MGraph mGraph = new MGraph(verxs);
+		MinTree minTree = new MinTree();
+		minTree.createGraph(mGraph, verxs, data, weight);
+		minTree.showGraph(mGraph);
+		minTree.prim(mGraph, 0);
+
+	}
+
+}
+
+class MinTree {
+	/**
+	 * @param graph  图对象
+	 * @param verxs  图对应的顶点个数
+	 * @param data   图的各个顶点的值
+	 * @param weight 图的临接矩阵
+	 */
+	public void createGraph(MGraph graph, int verxs, char data[], int[][] weight) {
+		int i, j;
+		for (i = 0; i < verxs; i++) {
+			graph.data[i] = data[i];
+			for (j = 0; j < verxs; j++) {
+				graph.weight[i][j] = weight[i][j];
+			}
+		}
+	}
+
+	public void showGraph(MGraph graph) {
+		for (int[] link : graph.weight) {
+			System.out.println(Arrays.toString(link));
+		}
+	}
+
+	public void prim(MGraph graph, int v) {
+		int visited[] = new int[graph.verxs];
+		visited[v] = 1;
+		int h1 = -1;
+		int h2 = -1;
+		int minWeight = 10000;
+		for (int k = 1; k < graph.verxs; k++) {
+			for (int i = 0; i < graph.verxs; i++) {
+				for (int j = 0; j < graph.verxs; j++) {
+					if (visited[i] == 1 && visited[j] == 0 && graph.weight[i][j] < minWeight) {
+						minWeight = graph.weight[i][j];
+						h1 = i;
+						h2 = j;
+					}
+				}
+			}
+			System.out.println("边<" + graph.data[h1] + "," + graph.data[h2] + ">权值" + minWeight);
+			visited[h2] = 1;
+			minWeight = 10000;
+		}
+	}
+}
+
+class MGraph {
+	int verxs;//表示图的节点个数
+	char[] data;//存放节点数据
+	int[][] weight;//存放边，临接矩阵
+
+	public MGraph(int verxs) {
+		this.verxs = verxs;
+		data = new char[verxs];
+		weight = new int[verxs][verxs];
+	}
+
+}
